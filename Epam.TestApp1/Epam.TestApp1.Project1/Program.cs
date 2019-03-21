d6392a098510f334b4209d07c3a0d70506505c7e
Roman Polikutkin <roman_polikutkin@epam.com>
2019-03-21 16:19:02 +0400

    5555

--- Epam.TestApp1/Epam.TestApp1.Project1/Program.cs
+++ Epam.TestApp1/Epam.TestApp1.Project1/Program.cs
@@ -11,6 +11,7 @@ namespace Epam.TestApp1.Project1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("im second");
             Console.ReadKey();
         }
     }
